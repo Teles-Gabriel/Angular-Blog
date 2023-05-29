f6446c958e625de301f2698bc112cb26bfa757b6
Teles-Gabriel <gabrielteles0609@gmail.com>
2023-05-29 20:13:06 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -12,6 +12,8 @@ export class SmallCardComponent implements OnInit {
   cardTitle:string = ""
   @Input()
   textDescription:string = ""
+  @Input()
+  Id:string = "0"
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/datafake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Somos diferentes e também somos iguais",
+        "description": "ola",
+        "photoCover": "https://uploads.spiritfanfiction.com/historias/capas/202010/obanai-x-mitsuri-20796133-201020201542.jpg"
+    },
+    {
+        "id":"2",
+        "title":"saiu novo filme",
+        "description": "ola",
+        "photoCover": ""
+    },
+    {
+        "id":"3",
+        "title":"saiu novo filme",
+        "description": "ola",
+        "photoCover": ""
+    }
+]
