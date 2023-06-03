401f90507b710671bad527bcfa9114c462fbc115
Teles-Gabriel <gabrielteles0609@gmail.com>
2023-06-03 20:40:55 -0300

    atualização 0.1.2

--- src/app/data/datafake.ts
+++ src/app/data/datafake.ts
@@ -20,7 +20,17 @@ export const dataFake = [
     {
         "id":"4",
         "title":"Pietra Rafaelle",
-        "description":"Por que o sorrisinho dela mexe tanto com o meu coração?Quero apenas cinco coisas...\n Primeiro é o amor sem fim \n A segunda é ver o outono \n A terceira é o grave inverno \ n Em quarto lugar o verão \n A quinta coisa são teus olhos \n Não quero dormir sem teus olhos. \n Não quero ser... sem que me olhes. \n Abro mão da primavera para que continues me olhando. Pablo Neruda ",
+        "description": `Por que o sorrisinho dela mexe tanto com o meu coração?
+        Quero apenas cinco coisas...
+         Primeiro é o amor sem fim
+          A segunda é ver o outono
+           A terceira é o grave inverno
+            Em quarto lugar o verão
+             A quinta coisa são teus olhos
+              Não quero dormir sem teus olhos. 
+               Não quero ser... sem que me olhes. 
+                Abro mão da primavera para que continues me olhando. 
+                -Pablo Neruda `,
         "photoCover":"https://scontent.fbsb12-2.fna.fbcdn.net/v/t39.30808-6/302429954_5441200942636002_4359077633441421743_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=k_7F8aF0wCIAX8SbRxA&_nc_ht=scontent.fbsb12-2.fna&oh=00_AfD7FHMCbopJxJXHMWDkuYM6ImTsgyiiwt72G22-XKYGUg&oe=6480FB52"
     }
 ]
