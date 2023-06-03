b1b82c47ffa4e40529306028ffc2c9a4af4f4372
Teles-Gabriel <gabrielteles0609@gmail.com>
2023-06-03 18:46:50 -0300

    finalização

--- src/app/data/datafake.ts
+++ src/app/data/datafake.ts
@@ -16,5 +16,11 @@ export const dataFake = [
         "title":"saiu novo filme",
         "description": "ola",
         "photoCover": ""
+    },
+    {
+        "id":"4",
+        "title":"Pietra Rafaelle",
+        "description":"Por que o sorrisinho dela mexe tanto com o meu coração?",
+        "photoCover":"https://scontent.fbsb12-2.fna.fbcdn.net/v/t39.30808-6/302429954_5441200942636002_4359077633441421743_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGdkdWEN_x1aaCtuQSQ9PCgC8LkrVJzq7ULwuStUnOrtRflMXl4ARODb_Y-wRhrBcdJeobdiLTHzxQsIMszvANw&_nc_ohc=X15VF2ZHgikAX_xmS4k&_nc_ht=scontent.fbsb12-2.fna&oh=00_AfDUPfV0JBuWAkCv_ASJvia_vbQla8Z1lGHTbt3-WQPeCw&oe=647B0C92"
     }
 ]
