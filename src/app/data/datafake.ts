891d68068e06d6d92171994e7e131d081a6918ed
Gabriel Teles <gabrielteles0609@gmail.com>
2024-06-18 15:55:53 -0300

    fix: content of cards

--- src/app/data/datafake.ts
+++ src/app/data/datafake.ts
@@ -1,36 +1,29 @@
 export const dataFake = [
     {
         "id":"1",
-        "title":"Somos diferentes e também somos iguais",
-        "description": "ola",
-        "photoCover": "https://uploads.spiritfanfiction.com/historias/capas/202010/obanai-x-mitsuri-20796133-201020201542.jpg"
+        "title":"Estagiários do ITI desenvolvem chat com criptografia avançada",
+        "description": `A criação do chat surgiu de um desafio proposto por Joelmo Oliveira, coordenador da CGICP, para que os estagiários entendessem a criptografia na prática e de forma didática. O desafio consistia em um jogo onde dois grupos distintos deveriam se comunicar com criptografia assimétrica e um terceiro grupo tentaria decifrar as mensagens trocadas. 
+        A tecnologia desenvolvida por Anderson Lima (23), Arthur Carneiro (24), Bruno Ricardo (20), Luca Clerot Aviani (20), Luís Braga (21), Gabriel Teles (23) e Miguel Moreira (22), sob a liderança de Thiago Krasauskas (39), levou por volta de 15 dias para ficar pronta e foi desenvolvida sem apoio de pesquisador ou grupo de pesquisa externo.`,
+        "photoCover": "https://www.gov.br/iti/pt-br/assuntos/noticias/indice-de-noticias/estagiarios-do-iti-desenvolvem-chat-com-criptografia-avancada/iti_materia_estagiarios-criptografia-avancada_banner-site.jpg/@@images/a06184d1-3fff-43ed-ab9b-7dc0404f930d.jpeg"
     },
     {
         "id":"2",
-        "title":"saiu novo filme",
-        "description": "ola",
-        "photoCover": ""
+        "title":"Validar",
+        "description": `O validar é o principal serviço de validação de assinaturas em documentos eletronicos do Brasil.
+        Atualmente faço parte do time como estagiário de desenvolvimento de software full stack.
+        `,
+        "photoCover": "https://cryptoid.com.br/wp-content/uploads/2022/07/ITI-logo-peq-310x207.jpg"
     },
     {
         "id":"3",
-        "title":"saiu novo filme",
-        "description": "ola",
-        "photoCover": ""
+        "title":"Protótipo de assinador e verificador",
+        "description": "Protótipo de um sistema de assinaturas eletrônicas desde a assinatura do documento, criação de chaves públicas e privadas, até a validação das assinaturas do documento.",
+        "photoCover": "https://media.licdn.com/dms/image/D4D2DAQHN6PRsgAVNcw/profile-treasury-image-shrink_800_800/0/1715363103749?e=1719342000&v=beta&t=kO02wiSwcBCWlttjCSGwC-L2ceb7YDdIuGrRvhRoG94"
     },
     {
         "id":"4",
-        "title":"Pietra Rafaelle",
-        "description": `Por que o sorrisinho dela mexe tanto com o meu coração?
-        Quero apenas cinco coisas...
-         Primeiro é o amor sem fim
-          A segunda é ver o outono
-           A terceira é o grave inverno
-            Em quarto lugar o verão
-             A quinta coisa são teus olhos
-              Não quero dormir sem teus olhos. 
-               Não quero ser... sem que me olhes. 
-                Abro mão da primavera para que continues me olhando. 
-                -Pablo Neruda `,
-        "photoCover":"https://scontent.fbsb12-2.fna.fbcdn.net/v/t39.30808-6/302429954_5441200942636002_4359077633441421743_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=k_7F8aF0wCIAX8SbRxA&_nc_ht=scontent.fbsb12-2.fna&oh=00_AfD7FHMCbopJxJXHMWDkuYM6ImTsgyiiwt72G22-XKYGUg&oe=6480FB52"
+        "title":"Desafio mensagens criptografadas",
+        "description": `A tecnologia desenvolvida por Anderson Lima (23), Arthur Carneiro (24), Bruno Ricardo (20), Luca Clerot Aviani (20), Luís Braga (21), Gabriel Teles (23) e Miguel Moreira (22), sob a liderança de Thiago Krasauskas (39), levou por volta de 15 dias para ficar pronta e foi desenvolvida sem apoio de pesquisador ou grupo de pesquisa externo. `,
+        "photoCover":"https://www.gov.br/iti/pt-br/assuntos/noticias/indice-de-noticias/estagiarios-do-iti-desenvolvem-chat-com-criptografia-avancada/iti_materia_estagiarios-criptografia-avancada_banner-site.jpg/@@images/a06184d1-3fff-43ed-ab9b-7dc0404f930d.jpeg"
     }
 ]
